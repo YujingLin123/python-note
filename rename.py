e84ebaa6f5e2173ad38af7f5fcefdf31e90b8a18
linyujing199488 <67212190+linyujing199488@users.noreply.github.com>
2021-01-09 20:02:03 +0800

    Create rename.py

---
+++ rename.py
@@ -0,0 +1,9 @@
+DataFrame.rename(mapper=None, index=None, columns=None, axis=None, copy=True, inplace=False, level=None)
+
+"""
+映射器，索引和列：字典值，键表示旧名称，值表示新名称。这些参数只能一次使用。
+axis:int或字符串值，“ 0”表示行，“ 1”表示列。
+copy:如果为True，则复制基础数据。
+inplace:如果为True，则在原始 DataFrame 中进行更改。
+level:用于在数据帧具有多个级别索引的情况下指定级别。
+"""
